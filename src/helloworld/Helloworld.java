b17c134bc271ab730b9514285f5cd7b82139acb3
Pascal Wang <pawang@redhat.com>
2017-11-08 13:24:09 +0800

    add loop

--- src/helloworld/Helloworld.java
+++ src/helloworld/Helloworld.java
@@ -1,8 +1,11 @@
 package helloworld;
 
 public class Helloworld {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         // Prints "Hello, World" to the terminal window.
-        System.out.println("Hello, World");
+        while (true) {
+          System.out.println("Hello, World");
+          Thread.sleep(10000);
+        }
     }
 }
