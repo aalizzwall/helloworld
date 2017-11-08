d3072dbc40a1b0cd350efd54ed01efd00b5c7aa9
Pascal Wang <pawang@redhat.com>
2017-11-08 10:47:43 +0800

    init

---
+++ src/helloworld/helloworld.java
@@ -0,0 +1,8 @@
+package helloworld;
+
+public class helloworld {
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World");
+    }
+}
