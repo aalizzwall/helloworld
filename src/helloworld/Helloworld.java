0da32e31e5059a841579fbf37f8452cca11e3b89
Pascal Wang <pawang@redhat.com>
2017-11-08 10:59:40 +0800

    rename and add main class for jar

--- src/helloworld/Helloworld.java
+++ src/helloworld/Helloworld.java
@@ -1,6 +1,6 @@
 package helloworld;
 
-public class helloworld {
+public class Helloworld {
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
         System.out.println("Hello, World");
